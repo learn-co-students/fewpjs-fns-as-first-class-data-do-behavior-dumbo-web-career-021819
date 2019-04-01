8d4b5603dcc988bf244b4b2da59138be7c94b263
1-8192 <allegranzia@gmail.com>
2019-04-01 18:51:14 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  let i = parseInt(string)
+
+  if (i < 12) {
+    return "Good Morning"
+  } else if (i > 12 && i < 17)  {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(inputString) {
+  document.getElementById('greeting').textContent = `${inputString}`
+}
